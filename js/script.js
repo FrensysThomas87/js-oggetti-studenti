2077039c6f8bb3bb91f498b38d37a2370d7aab7c
FrensysThomas87 <delta873@gmail.com>
2021-01-18 15:34:32 +0100

    fine secondo punto

--- js/script.js
+++ js/script.js
@@ -8,6 +8,36 @@ console.log(typeof studente);
 
 
 for(var key in studente){
-  alert('Le proprietà di studente sono: ' + key + ' ' + studente[key]);
+  // alert('Le proprietà di studente sono: ' + key + ' ' + studente[key]);
   console.log('Le proprietà di studente sono: ' + key + ' ' +  studente[key]);
+}
+
+var listaStudenti = [
+  {
+    'nome': 'Mario',
+    'cognome': 'Rossi',
+
+  },
+
+  {
+    'nome': 'Marco',
+    'cognome': 'Bianchi',
+
+  },
+
+  {
+    'nome': 'Gianluca',
+    'cognome': 'Mariani',
+
+  }
+
+]
+
+// for(var i = 0; i < listaStudenti.length; i++){
+//   console.log(listaStudenti[i]);
+// }
+
+for(var s in listaStudenti){
+  // alert('Le proprietà di studente sono: ' + key + ' ' + studente[key]);
+  console.log(listaStudenti[s]);
 }
