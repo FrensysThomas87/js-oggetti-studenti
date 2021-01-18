45ebb4a04bf2a267a78a19c3b5fa90ee61bb62da
FrensysThomas87 <delta873@gmail.com>
2021-01-18 17:03:14 +0100

    ultime modifiche

--- js/script.js
+++ js/script.js
@@ -1,58 +1,71 @@
-var studente = {
-  'nome': 'Francesco',
-  'cognome': 'Tomassini',
-  'età': '33'
-}
-
-console.log(typeof studente);
-
-
-for(var key in studente){
-  // alert('Le proprietà di studente sono: ' + key + ' ' + studente[key]);
-  console.log('Le proprietà di studente sono: ' + key + ' ' +  studente[key]);
-}
-
-var listaStudenti = [
-  {
-    'nome': 'Mario',
-    'cognome': 'Rossi',
-
-  },
-
-  {
-    'nome': 'Marco',
-    'cognome': 'Bianchi',
-
-  },
-
-  {
-    'nome': 'Gianluca',
-    'cognome': 'Mariani',
-
-  }
-
-]
-
-// for(var i = 0; i < listaStudenti.length; i++){
-//   console.log(listaStudenti[i]);
+// var studente = {
+//   'nome': 'Francesco',
+//   'cognome': 'Tomassini',
+//   'età': '33'
+// }
+//
+// console.log(typeof studente);
+//
+//
+// for(var key in studente){
+//   // alert('Le proprietà di studente sono: ' + key + ' ' + studente[key]);
+//   console.log('Le proprietà di studente sono: ' + key + ' ' +  studente[key]);
 // }
 
-for(var s in listaStudenti){
-  // alert('Le proprietà di studente sono: ' + key + ' ' + studente[key]);
-  console.log(listaStudenti[s]);
+var listaStudenti = {
+  'numStudenti': 6,
+  'students':[
+    {
+      'nome': 'Mario',
+      'cognome': 'Rossi',
+      'età': 20
+    },
+
+    {
+      'nome': 'Marco',
+      'cognome': 'Bianchi',
+      'età': 30
+    },
+
+    {
+      'nome': 'Gianluca',
+      'cognome': 'Mariani',
+      'età': 40
+    }
+  ]
+
+}
+
+
+var learner;
+
+for(var x = 0; x < listaStudenti.students.length; x++){
+
+  learner = listaStudenti.students[x];
+
+
+    // console.log(learner.nome);
+    // console.log(learner.cognome);
+
+    for(var s in learner){
+      console.log(s + ' ha valore ' + learner.name);
+    }
+
+
 }
 
 
 
-while(listaStudenti.length < 6){
-  var nuovoStudente = {};
-   nuovoStudente.nome = prompt('Inserisci il tuo nome');
 
-   nuovoStudente.cognome = prompt('Inserisci il tuo cognome');
-
-   nuovoStudente.età = parseInt(prompt('Inserisci la tua eta'));
-
-   listaStudenti.push(nuovoStudente);
-}
-
-console.log(listaStudenti);
+// while(listaStudenti.students.length < listaStudenti.numStudenti){
+//   var nuovoStudente = {};
+//    nuovoStudente.nome = prompt('Inserisci il tuo nome');
+//
+//    nuovoStudente.cognome = prompt('Inserisci il tuo cognome');
+//
+//    nuovoStudente.età = parseInt(prompt('Inserisci la tua eta'));
+//
+//    listaStudenti.push(nuovoStudente);
+// }
+//
+// console.log(listaStudenti);
